ad098e7eeafb9ab604c06a9218de16260eda9a35
RYOMAEKAWA <ryomaekawa@gmail.com>
2025-02-16 21:38:04 +0900

    update rmd files

--- R/copy_raw_count_data.R
+++ R/copy_raw_count_data.R
@@ -1,12 +1,17 @@
-#' Copy raw_count_data.csv to Working Directory
+#' Copy raw_count_data.csv to data folder in Working Directory
 #'
-#' This function copies the raw_count_data.csv file from the package to the current working directory.
+#' This function copies the raw_count_data.csv file from the package to a 'data' folder in the current working directory.
 #' @export
 copy_raw_count_data <- function() {
+  # 作業ディレクトリ内にdataフォルダを作成（なければ）
+  if (!dir.exists("input_data")) {
+    dir.create("input_data")
+  }
+
   file_path <- system.file("extdata", "raw_count_data.csv", package = "IFFSbio")
   if (file.exists(file_path)) {
-    file.copy(file_path, to = getwd())
-    message("raw_count_data.csv has been copied to your working directory.")
+    file.copy(file_path, to = file.path(getwd(), "data"))
+    message("raw_count_data.csv has been copied to the 'data' folder in your working directory.")
   } else {
     stop("File not found in the package.")
   }
