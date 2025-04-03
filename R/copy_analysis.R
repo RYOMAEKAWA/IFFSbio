6da4f748e601c57ee226eae760cd1d028b3b2de5
RYOMAEKAWA <ryomaekawa@gmail.com>
2025-04-03 14:55:13 +0900

    update to 0.3.0

--- R/copy_analysis.R
+++ R/copy_analysis.R
@@ -1,13 +1,17 @@
-#' Copy Analysis Rmd to Working Directory
+#' Copy Analysis Rmd and kihon.R to Working Directory
 #'
-#' This function copies the analysis.Rmd file from the package to the current working directory.
+#' This function copies the analysis.Rmd and kihon.R files from the package to the current working directory.
 #' @export
 copy_analysis <- function() {
-  file_path <- system.file("code", "analysis.Rmd", package = "IFFSbio")
-  if (file.exists(file_path)) {
-    file.copy(file_path, to = getwd())
-    message("analysis.Rmd has been copied to your working directory.")
-  } else {
-    stop("File not found in the package.")
+  files_to_copy <- c("analysis.Rmd", "kihon.R")
+
+  for (file_name in files_to_copy) {
+    file_path <- system.file("code", file_name, package = "IFFSbio")
+    if (file.exists(file_path)) {
+      file.copy(file_path, to = getwd(), overwrite = TRUE)
+      message(file_name, " has been copied to your working directory.")
+    } else {
+      warning(file_name, " was not found in the package.")
+    }
   }
 }
